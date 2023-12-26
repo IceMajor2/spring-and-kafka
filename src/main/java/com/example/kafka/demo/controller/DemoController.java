2abf3661d2cafcdab3cbe7e65241e7d51dbcaf5b
Marcin Startek <marcin.startek@programisci.eu>
2023-12-26 15:30:31 +0100

    Demonstration controller & listener

---
+++ src/main/java/com/example/kafka/demo/controller/DemoController.java
@@ -0,0 +1,31 @@
+package com.example.kafka.demo.controller;
+
+import com.example.kafka.demo.model.KafkaMessageExample;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api")
+@Slf4j
+@RequiredArgsConstructor
+public class DemoController {
+
+  private final KafkaTemplate<String, KafkaMessageExample> kafkaTemplate;
+
+  @PostMapping("/kafka-message")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void sendKafkaMessage(@RequestBody final KafkaMessageExample kafkaMessageExample) {
+    log.info("Sending a message to {}: {}", kafkaMessageExample.getTopic(), kafkaMessageExample);
+    kafkaTemplate.send(kafkaMessageExample.getTopic(), kafkaMessageExample);
+  }
+}
